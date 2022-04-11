1768bdd84f877880b755a47e84e8aa5e69e32dfe
mati-rodriguez80 <95318985+mati-rodriguez80@users.noreply.github.com>
2022-04-11 18:53:15 -0300

    Restrict actions from UI based on if user is logged in or not

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first # This is temporary until we have an authentication system
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
