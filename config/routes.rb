14e03104996b4d1e953e416bf61a6424da5bf204
mati-rodriguez80 <95318985+mati-rodriguez80@users.noreply.github.com>
2022-04-29 20:59:09 -0300

    Add categories controller and actions, create new categories, use functional tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  # Pages
   root 'pages#home'
   get 'about', to: 'pages#about'
+  # Articles
   resources :articles
+  # Users
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  # Sessions
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  # Categories
+  resources :categories, except: [:destroy]
 end
